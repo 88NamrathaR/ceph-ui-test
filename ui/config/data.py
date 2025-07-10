93634acae8b3aa5f28bcfdef003467062c6ff721
nrao <nrao@redhat.com>
2025-07-10 11:46:01 +0530

    logintestcase

---
+++ ui/config/data.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class DashboardConfig:
+    url: str
+    username: str
+    password: str
+
+TEST_CONFIG = DashboardConfig(
+    url="https://10.0.64.196:8443/",
+    username="admin",
+    password="admin@123"
+)
